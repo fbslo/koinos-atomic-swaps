759c69410a7799b824df5122c28e345a84dd0ff4
fbslo <admin@fbslo.net>
2022-11-04 13:44:45 +0100

    add koinos contract init

--- README.md
+++ README.md
@@ -3,6 +3,8 @@ Create transaction on Koinos with same ID and deposit tokens.
 
 Once both transactions are verified, sign message on Ethereum, which will unlock both Koinos and Ethereum tokens.
 
+Ethereum Swap must be created first;
+
 ```
 {
   secretHash: "",
--- ethereum/AtomicSwap.sol
+++ ethereum/AtomicSwap.sol
@@ -1,6 +1,9 @@
+//SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.13;
 
 interface IERC20 {
+  function transfer(address to, uint256 amount) external;
   function transferFrom(address from, address to, uint256 amount) external;
   function balanceOf(address user) external returns (uint256);
 }
@@ -15,30 +18,47 @@ contract HTLC {
     uint256 amount;
     uint256 expiration;
     uint256 createdAt;
+    bool finalized;
   }
   mapping (uint256 => Swap) public swaps;
 
-  uint256 public lockTime = 7 days;
+  uint256 public lockTime = 5 days;
 
   event SwapCreated(uint256 id);
   event SwapCanceled(uint256 id);
   event SwapCompleted(uint256 id);
 
   function createASwap(
-    uint256 id;
+    uint256 id,
     bytes32 unlockHash,
     address receiver,
     address token,
     uint256 amount
   ) external {
     require(swaps[id].creator == address(0), "ID not unique");
-    
+
     IERC20(token).transferFrom(msg.sender, address(this), amount);
-    swaps[id] = Swap(unlockHash, msg.sender, receiver, token, amount, block.timestamp + lockTime, block.timestamp);
+    swaps[id] = Swap(unlockHash, msg.sender, receiver, token, amount, block.timestamp + lockTime, block.timestamp, false);
     emit SwapCreated(id);
   }
 
-  function cancelSwap(){
+  function completeSwap(uint256 id, string memory secret) external {
+    require(swaps[id].expiration <= block.timestamp, "exipred");
+    require(swaps[id].finalized == false, "already finalized");
+    require(keccak256(abi.encodePacked(secret)) == swaps[id].unlockHash, "invalid secret");
 
+    swaps[id].finalized = true;
+    IERC20(swaps[id].token).transfer(swaps[id].receiver, swaps[id].amount);
+    emit SwapCompleted(id);
+  }
+
+  function cancelSwap(uint256 id) external {
+    require(msg.sender == swaps[id].creator, "not a creator");
+    require(block.timestamp > swaps[id].expiration, "not exipred");
+    require(swaps[id].finalized == false, "already finalized");
+
+    swaps[id].finalized = true;
+    IERC20(swaps[id].token).transfer(swaps[id].creator, swaps[id].amount);
+    emit SwapCanceled(id);
   }
 }
